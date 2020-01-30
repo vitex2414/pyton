07d1542ac8dffd2875498233c2113d563af7cbb0
vitex2414 <60479628+vitex2414@users.noreply.github.com>
2020-01-30 19:39:12 +0300

    labi1,2

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,19 @@
+print("введите строку ")
+s = input()
+l = len(s)
+m=0
+ind=0
+count=0
+for i in range (l):
+    if s[i] !=';':
+        count += 1
+    else:
+        if count > m:
+                m = count
+                ind = i - count 
+                count = 0
+if count > m:
+    m = count
+    ind = i - count +1
+print("результат")
+print(s[ind:ind+m])
